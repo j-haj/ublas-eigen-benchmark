e4609ea1e2b4b20a7e1f306916623c5ba240ea36
Jeff Hajewski <jeff.hajewski@gmail.com>
2017-07-25 18:53:34 -0500

    Creates CMakeLists and benchmark run file

---
+++ src/main.cc
@@ -0,0 +1,20 @@
+/* vim: set ts=2 sts=2 et sw=2 tw=80: */
+/**
+ * Boost::ublas vs Eigen
+ *
+ * Author: Jeff Hajewski
+ * Created: 7/25/2017
+ *
+ * These benchmarks compare two popular linear algebra libraries. The benchmarks
+ * are not comprehensive and should increase in coverage over time.
+ */
+
+#include <iostream>
+
+int main(int argc, char* argv[]) {
+
+  std::cout << "\tBoost::ublas vs. Eigen Benchmarks\n"
+            << "\t---------------------------------\n";
+
+  return EXIT_SUCCESS;
+}
